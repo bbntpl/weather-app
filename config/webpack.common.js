9910f071866a9f7fce3c992138296266213444f3
bvrbryn445 <law911012eqw@gmail.com>
2022-02-16 20:52:34 -0500

    Style: initial website design

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,21 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/template.html',
-			favicon: './src/images/favicon.ico',
-		}),
-		new CopyPlugin({
-			patterns: [
-				{ from: './src/images/logo.svg', to: './images' },
-				{ from: './src/images/search.svg', to: './images' },
-				{ from: './src/images/favicon.ico', to: './images' },
-			],
-		}),
+	entry: [
+		'./src/index.js',
 	],
 	output: {
 		path: path.resolve(__dirname, '..', 'dist'),
@@ -31,6 +21,26 @@ module.exports = {
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource',
 			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Daily Weather Report',
+			template: './src/template.html',
+			favicon: './src/images/favicon.ico',
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: './src/images/logo.svg', to: './images' },
+				{ from: './src/images/search.svg', to: './images' },
+				{ from: './src/images/favicon.ico', to: './images' },
+			],
+		}),
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new HtmlMinimizerPlugin(),
 		],
 	},
 };
